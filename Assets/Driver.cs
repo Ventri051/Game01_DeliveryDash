ebdfc8dd73c15df3880e9b2197e705dd956351c7
Ventri051 <ventri051@morrisville.edu>
2025-09-14 17:32:54 -0400

    Part 4

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -1,18 +1,33 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
-    float steerSpeed = .5f;
-    float moveSpeed = .1f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] float steerSpeed = .5f;
+    [SerializeField] float moveSpeed = .1f;
+    
+     void Update()
     {
+        if (Keyboard.current.wKey.isPressed)
+        {
+            Debug.Log("We are pushing foward");
+       }
+      else if (Keyboard.current.sKey.isPressed)
+        {
+            Debug.Log("We are pushing back");
+       }
+      
+      if (Keyboard.current.aKey.isPressed)
+        {
+            Debug.Log("We are pushing left");
+        }
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            Debug.Log("We are pushing right");
+        }
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+       
+       
         transform.Rotate(0, 0, steerSpeed);
         transform.Translate(0, moveSpeed, 0);
     }
