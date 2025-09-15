5e62f2658edb5aa00eab688c0f630ff3588ffa3d
Ventri051 <ventri051@morrisville.edu>
2025-09-14 20:29:41 -0400

    Part 5

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -8,27 +8,29 @@ public class Driver : MonoBehaviour
     
      void Update()
     {
+        float move = 0f;
+        float steer = 0f;
         if (Keyboard.current.wKey.isPressed)
         {
-            Debug.Log("We are pushing foward");
-       }
-      else if (Keyboard.current.sKey.isPressed)
+            move = 1f;
+        }
+        else if (Keyboard.current.sKey.isPressed)
         {
-            Debug.Log("We are pushing back");
-       }
+            move = -1f;
+        }
       
       if (Keyboard.current.aKey.isPressed)
         {
-            Debug.Log("We are pushing left");
+            steer = 1f;
         }
         else if (Keyboard.current.dKey.isPressed)
         {
-            Debug.Log("We are pushing right");
+            steer = -1f;
         }
         
        
        
-        transform.Rotate(0, 0, steerSpeed);
-        transform.Translate(0, moveSpeed, 0);
+        transform.Rotate(0, 0, steer * steerSpeed);
+        transform.Translate(0, moveSpeed * move, 0);
     }
 }
